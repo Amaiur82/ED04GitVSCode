45c0294bf50b7e62a52ada25eefd3b33fdd60986
amaiur <aberategi@birt.eus>
2026-05-13 13:33:42 +0200

    Version 1. despues de haberme vuelto loca

--- src/main/java/ed/ud04ejemplogit/UD04EjemploGit.java
+++ src/main/java/ed/ud04ejemplogit/UD04EjemploGit.java
@@ -3,12 +3,12 @@ package ed.ud04ejemplogit;
 
 /**
  * Descripcion de la clase esto es para SSH prueba 2 SSH
- * pruebas en VSCode
+ * pruebas 2 en VSCode
  * @author amaiur
  */
 public class UD04EjemploGit {
 
     public static void main(String[] args) {
-        System.out.println("Hola, caracola");
+        System.out.println("estoy hartaaa");
     }
 }
