fa893ead1f5827e9d72dc2d1b293a0fc6ad9fb73
amaiur <aberategi@birt.eus>
2026-05-13 12:47:03 +0200

    Primer programa en VS Code

---
+++ src/main/java/ed/ud04ejemplogit/UD04EjemploGit.java
@@ -0,0 +1,14 @@
+// Comentario
+package ed.ud04ejemplogit;
+
+/**
+ * Descripcion de la clase esto es para SSH prueba 2 SSH
+ * pruebas en VSCode
+ * @author amaiur
+ */
+public class UD04EjemploGit {
+
+    public static void main(String[] args) {
+        System.out.println("Hola, caracola");
+    }
+}
